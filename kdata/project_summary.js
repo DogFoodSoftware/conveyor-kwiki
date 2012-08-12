74c947f78b18aa958bb47b8fb94e9fc7575e9141
Zane Rockenbaugh <zane@liquid-labs.com>
2012-08-12 13:10:18 -0500

    project summary

---
+++ kdata/project_summary.js
@@ -0,0 +1,8 @@
+{
+    "title": "KibblesWiki",
+    "status": "alpha",
+    "audience": "kibbles developers",
+    "foundations": "kibbles, PHP",
+    "short description": "Simple RESTful wiki application used in Kibbles development.",
+    "url id": "kwiki"
+}
