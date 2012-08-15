af22b84618b2dcd4cdedcd4fe993c563117b58a1
Zane Rockenbaugh <zane@liquid-labs.com>
2012-08-15 10:02:33 -0500

    very basic kwiki doc retrieval

---
+++ src/documentation/controller.php
@@ -0,0 +1,11 @@
+<?php
+/**
+<div class="p">
+A standard Kibbles controller (TODO: link the REST Processing Pattern section of the docs) for documentation resources.
+</div>
+ */
+?>
+<?php
+$handlerBase = dirname(__FILE__);
+require('/home/user/playground/kibbles-core/runnable/lib/kibbles-controller.php');
+?>
---
+++ src/documentation/get_all_html.php
@@ -0,0 +1,20 @@
+<?php
+/**
+<div class="p">
+
+</div>
+*/
+?>
+<?php
+$requestUri = $_SERVER['REQUEST_URI'];
+
+// extract the project
+$project = preg_replace('/^\/documentation\/([^\/]+).*/', '$1', $requestUri);
+// extract the file path
+$filePath = preg_replace("/^\/documentation\/$project/", '', $requestUri);
+$baseDir = '/home/user/playground';
+// now put it all together
+$filePath = "$baseDir/$project/kdata/kwiki/$filePath";
+
+echo file_get_contents($filePath);
+?>
