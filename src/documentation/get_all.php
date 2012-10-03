27647ae639c6e1a851752710f7ff9cca541f746d
Zane Rockenbaugh <zane@liquid-labs.com>
2012-10-03 00:57:10 -0500

    update to use new PHP, fix the directory processing to exclude build and data properly

--- src/documentation/get_all.php
+++ src/documentation/get_all.php
@@ -14,37 +14,43 @@ if (preg_match('/application\/json/', $requested_format)) { // good to go
     while (count($frontier) > 0) {
 	$dir_to_explore = array_shift($frontier);
 	$dh = opendir($dir_to_explore);
+	// we start out processing the projects, after the first dir, we're in
+	// the projects and our frontier filter logic changes
+	$in_projects = false;
 	while (false !== ($file = readdir($dh))) {
 	    if (is_dir("$dir_to_explore/$file")) {// add dirs to frontier
 		if ($file != '.' && $file != '..' && // skip special directories
 		    $file != '.git' && // skip git repos
-		    $file != 'data') // skip data dirs
+		    ($in_projects || 
+		     ($file != 'data' && // skip data dirs
+		      $file != 'build' && // skip build dirs
+		      $file != 'runnable'))) // and runnable
 		    array_unshift($frontier,"$dir_to_explore/$file");
 	    }
 	    else {// add URI path that corresponds to file to result
 		// first, strip the base
 		$file_path = substr("$dir_to_explore/$file", strlen('/home/user/playground'));
 		$project = preg_replace('/^\/([^\/]+).*/', '$1', $file_path);
-		$file_path = preg_replace('/^\/[^\/]+/', '', $file_path);
+		$file_path = preg_replace('/^\/[^\/]+\//', '', $file_path);
 		// There are three kinds of files we need to deal with:
 		// 1) Code files we know how to process:
 		if (preg_match('/(\.php|\.js)$/', $file_path))
 		    array_push($files, "/documentation/$project/$file_path");
 		// 2) Wiki pages
 		else if (preg_match('/\/[^\/\.]+$/', $file_path))
-		     array_push($files, "/documentation/$project/$file_path");
+		    array_push($files, "/documentation/$project/$file_path");
 		// otherwise it's a document we can't yet process, so it gets dropped
 		/**
 		   <todo>We should log a warning for dropped files.</todo>
 		 */
 	    }
 	}
+
+	// after the first iteration, we are in projects
+	$in_projects = true;
     }
 
-    /**
-       <todo>Use the JSON_PRETTY_PRINT and JSON_UNESCAPED_SLASHES once we upgrade to PHP 5.4+.</tode>
-     */
-    echo json_encode($files); //, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+    echo json_encode($files, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 }
 else header("HTTP/1.0 406 Cannot satisfy requested response format.");
 ?>
