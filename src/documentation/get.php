e95d8be5135f5c322cc89d5963e8cf44b16e0d28
Zane Rockenbaugh <zane@liquid-labs.com>
2012-11-06 23:52:55 -0600

    fixed variable names to match convention

--- src/documentation/get.php
+++ src/documentation/get.php
@@ -24,12 +24,12 @@
   footer template.
 </div>
  */
-$file_path = $_SERVER['REQUEST_URI'];
+$rest_id = $_SERVER['REQUEST_URI'];
 /**
 <todo>We are using regexp here for brevity, but it's probably more efficient
  to use substr_compare: http://stackoverflow.com/questions/619610/whats-the-most-efficient-test-of-whether-a-php-string-ends-with-another-string</todo>
  */
-if (preg_match('/(.php|.js)$/', $file_path))
+if (preg_match('/(.php|.js)$/', $rest_id))
     require '/home/user/playground/kwiki/runnable/lib/code_processor.php';
 else // it's a 'standard wiki page'
     require '/home/user/playground/kwiki/runnable/lib/wiki_page_processor.php';
